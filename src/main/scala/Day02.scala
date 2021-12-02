91c4e77fe03a1928e6142b3af2ef1392ee5ce9a9
Fleur Kelpin <f.kelpin@umcg.nl>
2021-12-02 11:01:38 +0100

    feat: day 2 part 2

--- src/main/scala/Day02.scala
+++ src/main/scala/Day02.scala
@@ -2,13 +2,22 @@ import common.loadPackets
 
 object Day02 extends App {
 
-  case class Position(horizontal: Int = 0, depth: Int = 0) {
+  case class Position(horizontal: Int = 0, depth: Int = 0, aim: Int = 0) {
     def move: Instruction => Position = {
       case Instruction(direction, amount) =>
         direction match {
           case "up" => copy(depth = depth - amount)
           case "down" => copy(depth = depth + amount)
           case "forward" => copy(horizontal = horizontal + amount)
+        }
+    }
+
+    def moveWithAim: Instruction => Position = {
+      case Instruction(direction, amount) =>
+        direction match {
+          case "up" => copy(aim = aim - amount)
+          case "down" => copy(aim = aim + amount)
+          case "forward" => copy(horizontal = horizontal + amount, depth = depth + aim * amount)
         }
     }
   }
@@ -22,4 +31,7 @@ object Day02 extends App {
   val part1 = input.foldLeft(Position())((position, instruction) => position.move(instruction))
   println(part1.horizontal * part1.depth)
 
+  val part2 = input.foldLeft(Position())((position, instruction) => position.moveWithAim(instruction))
+  println(part2)
+  println(part2.horizontal * part2.depth)
 }
